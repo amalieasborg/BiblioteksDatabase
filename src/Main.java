d0a075c37d66a04a126f015e62e057b36bac3033
Amalie <amalieasborg@gmail.com>
2023-11-15 14:21:09 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        DbSql db=new DbSql();
+        db.soegBog(1);
+        db.udlaanBog(1,2,"201123");
+        //db.soegLaaner(1);
+        //db.alleBoeger();
+        //db.alleLaanere();
+        //db.udlaanBog(1,2,"201123");
+        // db.udlaanBog(2,2,"201123");
+        /*Laener l=db.laanerOpl1(2);
+        System.out.println(l.getFnavn()+" "+l.getEnavn()+" har lånt følgende bøger:");
+        ArrayList<Bog> liste=l.getLaenteBoeger();
+        for(int i=0;i< liste.size();i++){
+            System.out.println(liste.get(i));
+        }*/
+    }
+}
